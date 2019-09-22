e1a7e7f321ca9d8c1ec21d5038f4bb110e3369f7
scherzer <scherzer@hs-weingarten.de>
2019-09-22 16:21:45 +0200

    quick commit

--- GLSLhelper/RegexPatterns.cs
+++ GLSLhelper/RegexPatterns.cs
@@ -4,8 +4,28 @@ namespace GLSLhelper
 {
 	public static class RegexPatterns
 	{
-		public static string BlockComment => $@"{Regex.Escape("/*")}(.|[\r\n])*?{Regex.Escape("*/")}"; //match everything till end block comment
-		public static string Include => @"#include\s+""([^""]+)"""; //match everything inside " except " so we get shortest ".+" match
-		public static string Uniform => @"uniform\s+([^\s]+)\s+([^\s]+)\s*;"; //matches uniform<spaces>type<spaces>name<spaces>; 
+		/// <summary>
+		/// Matches everything till end block comment into a group
+		/// </summary>
+		/// <value>
+		/// Regular expression.
+		/// </value>
+		public static string BlockComment => $@"{Regex.Escape("/*")}(.|[\r\n])*?{Regex.Escape("*/")}";
+
+		/// <summary>
+		/// Matches everything inside #include "(.)" except " so we get shortest ".+" match into a group
+		/// </summary>
+		/// <value>
+		/// Regular expression.
+		/// </value>
+		public static string Include => @"#include\s+""([^""]+)"""; //
+
+		/// <summary>
+		/// Matches uniform<spaces>type<spaces>name<spaces>; into two groups 
+		/// </summary>
+		/// <value>
+		/// Regular expression.
+		/// </value>
+		public static string Uniform => @"uniform\s+([^\s]+)\s+([^\s]+)\s*;"; 
 	}
 }
